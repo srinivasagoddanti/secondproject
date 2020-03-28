cee48dc2e67dcdd7a4c6a974bcc254ace1150c0b
srinivasagoddanti <srinivasa.goddanti@nihilent.com>
2020-03-28 14:42:18 +0530

    sample project

---
+++ src/main/scala/Demo/Demo.scala
@@ -0,0 +1,11 @@
+package Demo
+
+object Demo {
+  def main(args: Array[String]) {
+    functionExample()           // Calling function
+  }
+  def functionExample()  {        // Defining a function
+    println("This is a simple function")
+  }
+
+}
